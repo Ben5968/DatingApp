02641e1597e229fc7d6657f8895f22d5758bd027
unknown <acct5857@gmail.com>
2019-06-27 17:32:49 +0100

    AutoMapper

--- DatingApp.API/Helpers/Extensions.cs
+++ DatingApp.API/Helpers/Extensions.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Http;
 
 namespace DatingApp.API.Helpers
@@ -9,6 +10,17 @@ namespace DatingApp.API.Helpers
             response.Headers.Add("Application-Error", message);
             response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
             response.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+
+        public static int CalculateAge(this DateTime theDateTime)
+        {
+            var today = DateTime.Today;
+            // Calculate the age.
+            var age = today.Year - theDateTime.Year;
+            // Go back to the year the person was born in case of a leap year
+            if (theDateTime.Date > today.AddYears(-age)) age--;
+
+            return age;
         }
     }
 }
